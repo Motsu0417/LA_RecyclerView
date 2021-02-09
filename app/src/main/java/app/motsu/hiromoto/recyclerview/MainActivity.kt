60c0afe92129aed6be33f5c44d08df518f0ab62b
Motsu0417 <pippi9400@gmail.com>
2021-02-10 03:31:27 +0900

    FirstCommit

--- app/src/main/java/app/motsu/hiromoto/recyclerview/MainActivity.kt
+++ app/src/main/java/app/motsu/hiromoto/recyclerview/MainActivity.kt
@@ -2,10 +2,22 @@ package app.motsu.hiromoto.recyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     val courseDatas: List<CourseData> = listOf(
+        CourseData(R.drawable.android, "Androidアプリプログラミングコース", "色んなアプリを作って、プログラミングを楽しもう"),
+        CourseData(R.drawable.iphone, "iPhoneアプリプログラミングコース", "世界に一つだけiPhoneアプリを作って、周りを驚かせよう！"),
+        CourseData(R.drawable.android, "Androidアプリプログラミングコース", "色んなアプリを作って、プログラミングを楽しもう"),
+        CourseData(R.drawable.iphone, "iPhoneアプリプログラミングコース", "世界に一つだけiPhoneアプリを作って、周りを驚かせよう！"),
+        CourseData(R.drawable.android, "Androidアプリプログラミングコース", "色んなアプリを作って、プログラミングを楽しもう"),
+        CourseData(R.drawable.iphone, "iPhoneアプリプログラミングコース", "世界に一つだけiPhoneアプリを作って、周りを驚かせよう！"),
+        CourseData(R.drawable.android, "Androidアプリプログラミングコース", "色んなアプリを作って、プログラミングを楽しもう"),
+        CourseData(R.drawable.iphone, "iPhoneアプリプログラミングコース", "世界に一つだけiPhoneアプリを作って、周りを驚かせよう！"),
+        CourseData(R.drawable.android, "Androidアプリプログラミングコース", "色んなアプリを作って、プログラミングを楽しもう"),
+        CourseData(R.drawable.iphone, "iPhoneアプリプログラミングコース", "世界に一つだけiPhoneアプリを作って、周りを驚かせよう！"),
         CourseData(R.drawable.android, "Androidアプリプログラミングコース", "色んなアプリを作って、プログラミングを楽しもう"),
         CourseData(R.drawable.iphone, "iPhoneアプリプログラミングコース", "世界に一つだけiPhoneアプリを作って、周りを驚かせよう！"),
         CourseData(R.drawable.unity, "Unityゲームプログラミングコース", "世界を驚かすワクワクが止まらない！3Dゲームを制作しよう!")
@@ -13,5 +25,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val adapter = RecyclerViewAdapter(this)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
+
+        adapter.addAll(courseDatas)
     }
 }
